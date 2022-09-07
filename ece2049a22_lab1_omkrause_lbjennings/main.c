02ddcb9c1b7ba3308c28ef5e95ac5b5e2b51ae24
Owen Krause <28845074+Owen-K@users.noreply.github.com>
2022-09-07 09:43:40 -0400

    added countdown

--- ece2049a22_lab1_omkrause_lbjennings/main.c
+++ ece2049a22_lab1_omkrause_lbjennings/main.c
@@ -19,7 +19,6 @@ void main(void)
 {
     unsigned char currKey=0, dispSz = 3;
     unsigned char dispThree[3];
-    //unsigned char state = 0;
 
     enum GameState state;
     state = Welcome;
@@ -38,17 +37,37 @@ void main(void)
         switch(state)
         {
         case Welcome://welcome screen
-            Graphics_drawStringCentered(&g_sContext, "Welcome", AUTO_STRING_LENGTH, 48, 15, TRANSPARENT_TEXT);
+            Graphics_drawStringCentered(&g_sContext, "SPACE INVADERS", AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT);
             Graphics_flushBuffer(&g_sContext);
+
+            state = CheckKeypad;
 
             break;
         case CountDown://count down
+            Graphics_clearDisplay(&g_sContext);
+            Graphics_drawStringCentered(&g_sContext, "3", AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT);
+            Graphics_flushBuffer(&g_sContext);
+            swDelay(1);
+            Graphics_clearDisplay(&g_sContext);
+            Graphics_drawStringCentered(&g_sContext, "2", AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT);
+            Graphics_flushBuffer(&g_sContext);
+            swDelay(1);
+            Graphics_clearDisplay(&g_sContext);
+            Graphics_drawStringCentered(&g_sContext, "1", AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT);
+            Graphics_flushBuffer(&g_sContext);
+            swDelay(1);
+
+            state = DrawAliens;
             //count down to game start trigger alien drawing
             break;
         case DrawAliens://draw aliens
             //draw aliens on screen and move them
             break;
         case CheckKeypad://check keypad
+            currKey = getKey();
+
+            if(currKey == '*')
+                state = CountDown;
             //if(key == '*') {state = newGame; }
             //check keypad for button presses and remove appropriate aliens
             break;
@@ -61,4 +80,25 @@ void main(void)
         }
     }
 
+}
+
+void swDelay(char numLoops)
+{
+    // This function is a software delay. It performs
+    // useless loops to waste a bit of time
+    //
+    // Input: numLoops = number of delay loops to execute
+    // Output: none
+    //
+    // smj, ECE2049, 25 Aug 2021
+
+    volatile unsigned int i,j;  // volatile to prevent removal in optimization
+                                // by compiler. Functionally this is useless code
+
+    for (j=0; j<numLoops; j++)
+    {
+        i = 50000 ;                 // SW Delay
+        while (i > 0)               // could also have used while (i)
+           i--;
+    }
 }
