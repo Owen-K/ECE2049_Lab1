9ad77f1d5f7adf8d54c0bebdd22c7921ab373389
E-maiL981 <72767491+E-maiL981@users.noreply.github.com>
2022-09-14 21:52:23 -0400

    added comments

--- ece2049a22_lab1_omkrause_lbjennings/main.c
+++ ece2049a22_lab1_omkrause_lbjennings/main.c
@@ -10,14 +10,38 @@
 
 #define ALIENS_ROW 10
 #define ALIENS_COL 5
-#define LOOP_MAX 4000 //40,000
+#define LOOP_MAX 4000 //4,000
 
 // Function Prototypes
 void swDelay(char numLoops);
 
+void swDelay(char numLoops)
+{
+    // This function is a software delay. It performs
+    // useless loops to waste a bit of time
+    //
+    // Input: numLoops = number of delay loops to execute
+    // Output: none
+    //
+    // smj, ECE2049, 25 Aug 2021
+
+    volatile unsigned int i,j;  // volatile to prevent removal in optimization
+                                // by compiler. Functionally this is useless code
+
+    for (j=0; j<numLoops; j++)
+    {
+        i = 50000 ;                 // SW Delay
+        while (i > 0)               // could also have used while (i)
+           i--;
+    }
+}
+
 // Declare globals here
+
+//enumeration of all the gamestates
 enum GameState{Welcome, CheckStart, CountDown, MoveAliens, GenAliens, DrawAliens, CheckEnd, CheckKeypad, GameOver, NewLevel};
 
+//method to reset the aliens array
 void resetAliens(unsigned char aliens[ALIENS_ROW][ALIENS_COL]){
     int i, j;
     for(i = 0; i < ALIENS_ROW; i++){
@@ -27,6 +51,7 @@ void resetAliens(unsigned char aliens[ALIENS_ROW][ALIENS_COL]){
     }
 }
 
+//scans up a column to see if has a
 int shootColumn(unsigned char currKey, unsigned char aliens[ALIENS_ROW][ALIENS_COL]){
     int i;
     int j = currKey - '1';
@@ -44,34 +69,38 @@ int shootColumn(unsigned char currKey, unsigned char aliens[ALIENS_ROW][ALIENS_C
 void main(void)
 
 {
-    unsigned char currKey=0, dispSz = 3;
-    unsigned char dispThree[3];
+    unsigned char currKey=0;//, dispSz = 3;
+    //unsigned char dispThree[3];
 
+    //array of characters representing the aliens
     unsigned char aliens[ALIENS_ROW][ALIENS_COL];
 
-
+    //enumeration representing game state, initially set to display welcome screen.
     enum GameState state;
     state = Welcome;
 
     WDTCTL = WDTPW | WDTHOLD;    // Stop watchdog timer. Always need to stop this!!
                                  // You can then configure it properly, if desired
 
-
+    //config methods
     configDisplay();
     configKeypad();
 
     Graphics_clearDisplay(&g_sContext); // Clear the display
     int i , j; //loop variables used at various points
-    int level = 1;
-    int numAliens = 0;
+    int level =     1; //level representing current difficulty
+    int numAliens = 0; //amount of aliens created during current level
     int maxAliens = 5;
-    int numShot   = 0;
+    int numShot   = 0; //amount of aliens successfully eliminated
+
+    //long int counting the number of loops passed through
     long unsigned int loopCounter = 0;
+
     while(1)
     {
         switch(state)
         {
-        case Welcome://welcome screen
+        case Welcome://displays welcome screen
             Graphics_clearDisplay(&g_sContext);
             Graphics_drawStringCentered(&g_sContext, "SPACE INVADERS", AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT);
             Graphics_flushBuffer(&g_sContext);
@@ -79,12 +108,16 @@ void main(void)
 
             break;
         case CheckStart:
+            //checks to see if * key has been pressed to start
+            //distinct from CheckKeypad to avoid being triggered by the loop counter
             if(getKey() == '*'){
                 loopCounter = 0;
                 state = CountDown;
             }
             break;
         case CountDown:
+            //resets game state
+            //display 3 2 1 countdown
             resetAliens(aliens);
             numAliens = 0;
             maxAliens = level * 5;
@@ -108,7 +141,10 @@ void main(void)
             break;
 
         case MoveAliens:
-
+            //moves all aliens down one row.
+            //works by overwriting the one below with the one above
+            //first row is an edge case: is left unchanged, duplicated below
+            //designed to be overwritten in next function
             for(i = ALIENS_ROW-2; i > 0; i--){
                 for(j = 0; j<ALIENS_COL; j++){
                     aliens[i][j] = aliens[i-1][j];
@@ -119,6 +155,8 @@ void main(void)
             break;
 
         case GenAliens:
+            //randomly generates aliens on first row of screen
+            //see lab report for detailed explanation on how it works
             for(i = 0; i < ALIENS_COL; i++){
                 if(rand() % 5 + 1 <= level && numAliens < maxAliens){
                     aliens[0][i] = '1' + i;
@@ -132,9 +170,12 @@ void main(void)
             break;
 
         case CheckEnd:
+            //checks end conditions of program
 
+            //checks to see if game over has been reached
+            //scans bottom row to see if any aliens have reached it
             for (i = 0; i < ALIENS_COL; i++){
-                if(aliens[ALIENS_ROW-2][i] != 0){ //&& aliens[ALIENS_ROW-1][i] != '0'){
+                if(aliens[ALIENS_ROW-2][i] != 0){
                     state = GameOver;
                     break;
                 }else{
@@ -142,6 +183,8 @@ void main(void)
                 }
             }
 
+            //checks if player has shot all of the aliens
+            //moves to next level if so
             if(numShot == maxAliens)
                 state = NewLevel;
 
@@ -150,12 +193,18 @@ void main(void)
         case DrawAliens://draw aliens
             Graphics_clearDisplay(&g_sContext);
 
+            //goes through entire array and prints out each character
+            //characters are spaced 16 pixels apart horizontally
+            //characters are spaced 8 characters apart vertically, with a initial vertical offset of 4 pixels
+            //screen is 96 x 96 pixels, we thought this was appropriate
             for(i = 0; i < ALIENS_ROW; i++){
                 for(j = 0; j<ALIENS_COL; j++ ){
+                    //function only accepts a string, will not print properly if null terminator is not present
                     unsigned char curr[2] = aliens[i][j] + '\0';
 
                     //16 = horizontal spacer
                     //8  = vertical spacer
+                    //4 = initial vertical offset
                     Graphics_drawStringCentered(&g_sContext, curr, AUTO_STRING_LENGTH, 16 * (j+1), 4 + i * 8, TRANSPARENT_TEXT);
                 }
             }
@@ -167,6 +216,7 @@ void main(void)
 
 
         case CheckKeypad://check keypad, usual state for program
+            //checks if time to update aliens
             if(loopCounter > LOOP_MAX/level){
                 state = MoveAliens;
                 loopCounter = 0;
@@ -175,9 +225,12 @@ void main(void)
 
             currKey = getKey();
 
+            //goes through possible entries for a key pressed
             if(currKey == '*')
+                //resets and starts new game
                 state = CountDown;
             else if(currKey <= '5' && currKey >= '1'){
+                //shoots up
                 int k = shootColumn(currKey, aliens);
                 numShot += k;
                 for (i = 0; i < ALIENS_COL; i++){
@@ -191,22 +244,26 @@ void main(void)
             break;
 
         case GameOver://game over
+            //displays game over screen
             Graphics_clearDisplay(&g_sContext);
             Graphics_drawStringCentered(&g_sContext, "GAME OVER", AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT);
             Graphics_drawStringCentered(&g_sContext, "YOU SUCK", AUTO_STRING_LENGTH, 48, 58, TRANSPARENT_TEXT);
             Graphics_flushBuffer(&g_sContext);
 
             swDelay(3);
+            //reset level
             level = 1;
             state = Welcome;
             //display game over and allow for restart
             break;
         case NewLevel:
+            //level completed, display screen and increase level
             Graphics_clearDisplay(&g_sContext);
             Graphics_drawStringCentered(&g_sContext, "YOU WIN", AUTO_STRING_LENGTH, 48, 48, TRANSPARENT_TEXT);
             Graphics_drawStringCentered(&g_sContext, "NEW LEVEL", AUTO_STRING_LENGTH, 48, 58, TRANSPARENT_TEXT);
             Graphics_flushBuffer(&g_sContext);
             swDelay(3);
+            //increment level
             level++;
             state = CountDown;
             break;
@@ -215,25 +272,4 @@ void main(void)
             loopCounter++;
     }
 
-}
-
-void swDelay(char numLoops)
-{
-    // This function is a software delay. It performs
-    // useless loops to waste a bit of time
-    //
-    // Input: numLoops = number of delay loops to execute
-    // Output: none
-    //
-    // smj, ECE2049, 25 Aug 2021
-
-    volatile unsigned int i,j;  // volatile to prevent removal in optimization
-                                // by compiler. Functionally this is useless code
-
-    for (j=0; j<numLoops; j++)
-    {
-        i = 50000 ;                 // SW Delay
-        while (i > 0)               // could also have used while (i)
-           i--;
-    }
 }
